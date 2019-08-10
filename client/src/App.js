e93316f403ee0f68537ffad1910a2c53e2f8c3e3
Asha <ashalbenjamin@gmail.com>
2019-08-09 20:38:22 -0500

    updates-link-not-working

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,31 @@
 import React, { useState } from 'react';
+import { Route, Link } from "react-router-dom";
 
 import SavedList from './Movies/SavedList';
+import Movie from './Movies/Movie';
+import MovieList from './Movies/MovieList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
-  const addToSavedList = movie => {
+  const addSavedList = movie => {
     setSavedList( [...savedList, movie] );
   };
 
-  return (
-    <div>
+    return (
+    <div className="App">
+      <ul className="navbar">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path="/" component={MovieList} />
+        <Route  path="/movies/:id" component ={Movie} />
+      </div>
     </div>
-  );
-};
+    );
+  };
 
 export default App;
